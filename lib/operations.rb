49b1973848166f326b0714fa1c18fe36dc90622e
Lawrence Hon <lhon001@gmail.com>
2018-03-29 21:16:59 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+  if speed > 40 and speed < 60 
+    return false 
+  end
+  return true
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 40 and speed < 60? false:true
+	speed < 40 or speed > 60? true:false
 end
+
 	
 
 
