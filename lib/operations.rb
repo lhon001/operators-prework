3d84cbe4b6793684ff948b8c3226ee5479feea30
Lawrence Hon <lhon001@gmail.com>
2018-03-29 21:15:45 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,8 +8,8 @@ end
 
 
 def not_safe?(speed)
-	speed > 40 and speed < 60? false:true
-	speed < 40 and speed > 60? true:false
+	speed > 40 and speed < 60 ? false:true
+	speed < 40 and speed > 60 ? true:false
 	
 end
 
